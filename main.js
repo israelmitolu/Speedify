036c956547efff7b872bc832996ae1d5b7ca66d8
israelmitolu <israelmitolu@gmail.com>
2021-02-12 21:34:23 +0100

    Added the services section

--- main.js
+++ main.js
@@ -57,7 +57,7 @@ function scrollHeader(){
 }
 window.addEventListener('scroll', scrollHeader)
 
-/*==================== SHOW SCROLL TOP ====================*/ 
+/*============== SHOW SCROLL TOP ==================*/ 
 function scrollTop(){
     const scrollTop = document.getElementById('scroll-top');
     // When the scroll is higher than 560 viewport height, add the show-scroll class to the a tag with the scroll-top class
